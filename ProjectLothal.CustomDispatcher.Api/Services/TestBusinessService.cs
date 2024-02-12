c812c10de4a2fb07750396d2b65a9f56e178b2d7
utkudemr <utkuudemr@gmail.com>
2024-02-12 22:05:22 +0300

    test business service added

---
+++ ProjectLothal.CustomDispatcher.Api/Services/TestBusinessService.cs
@@ -0,0 +1,15 @@
+﻿namespace ProjectLothal.CustomDispatcher.Api.Services
+{
+    public class TestBusinessService : ITestBusinessService
+    {
+        public void TestMethod()
+        {
+            Console.Write("Helü");
+        }
+    }
+
+    public interface ITestBusinessService
+    {
+        void TestMethod();
+    }
+}
